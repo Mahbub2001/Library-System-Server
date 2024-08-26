81da4f1c199c1f91c0db31b80fb2b0991b926c5b
Mahbub2001 <imahbubturza@gmail.com>
2024-08-26 23:37:55 +0600

    update

--- account/views.py
+++ account/views.py
@@ -61,7 +61,8 @@ class UserRegistrationApiView(APIView):
             user = serializer.save()
             token = default_token_generator.make_token(user)
             uid = urlsafe_base64_encode(force_bytes(user.pk))
-            confirm_link = f"http://127.0.0.1:8000/account/active/{uid}/{token}"
+            confirm_link = f"https://library-system-server-nine.vercel.app/account/active/{uid}/{token}"
+            # confirm_link = f"http://127.0.0.1:8000/account/active/{uid}/{token}"
             email_subject = "Confirm Your Email"
             email_body = render_to_string('confirm_email.html', {'confirm_link': confirm_link})
 
